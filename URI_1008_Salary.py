cf94670eebe62b82676c1e7e7dee82ee5caaa719
Fazle Rabbi <94907550+rabbi303@users.noreply.github.com>
2022-07-03 10:07:19 +0600

    URI_1008_Salary.py
    Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
    
    Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
    Don’t forget the space before and after the equal signal and after the U$.
    Input
    The input file contains 2 integer numbers and 1 value of floating point, representing the number, worked hours amount and the amount the employee receives per worked hour.
    
    Output
    Print the number and the employee's salary, according to the given example, with a blank space before and after the equal signal.

---
+++ URI_1008_Salary.py
@@ -0,0 +1,9 @@
+# URI 1008
+number = int(input())
+worked_hours = int(input())
+receives_per_hour = float(input())
+
+salary = (worked_hours*receives_per_hour)
+
+print("NUMBER = %d" %number)
+print("SALARY = U$ %0.2f" %salary)
