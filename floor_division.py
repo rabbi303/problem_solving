b3e8010770f9aeecbdab4305ad5cd4367eb08084
Fazle Rabbi <94907550+rabbi303@users.noreply.github.com>
2023-12-25 10:44:07 +0600

    floor_division.py

--- floor_division.py
+++ floor_division.py
@@ -1,3 +1,4 @@
+#Floor division means the integer part of a division operation. For example, if you divide 17/5 the quotient will be 3.4.
 def floor_division(num1,num2):
  result_floor= num1//num2 
  result_ceil= -(-num1//num2)
