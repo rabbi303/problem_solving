900305147fe478237875ac6694382ad0f84c9cb6
Fazle Rabbi <94907550+rabbi303@users.noreply.github.com>
2023-12-26 11:39:51 +0600

    User input to Number
    Take two inputs from the user. One will be an integer. The other will be a float number. Then multiply them to display the output.

---
+++ User_input_to_Number.py
@@ -0,0 +1,12 @@
+def multiplying(num1,num2):
+    result = num1*num2
+    return result
+    
+num1 = int(input("Enter 1st number : "))
+num2 = float(input("Enter 2nd number : "))
+
+multi = multiplying(num1,num2)
+
+print(f"the multiplying of {num1} and {num2} are : {multi}")
+
+
